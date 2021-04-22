1ee37d9c354ef144ba231fdd38c3f9b27c361224
mengx520 <mengx520@gmail.com>
2021-04-21 17:53:28 -0700

    Modify lists
    Create toggles on list
    
    Modify display style

--- todoapp/app.py
+++ todoapp/app.py
@@ -24,6 +24,7 @@ class Todo(db.Model):
   description = db.Column(db.String(), nullable=False)
   completed = db.Column(db.Boolean, nullable=False, default=False)
   list_id = db.Column(db.Integer, db.ForeignKey('todolists.id'), nullable=False)
+
   
   def __repr__(self):
         return f'<Todo {self.id} {self.description}>'
@@ -80,8 +81,15 @@ def set_completed_todo(todo_id):
   return redirect(url_for('index'))
 
 @app.route('/lists/<list_id>')
+def get_list_todos(list_id):
+    return render_template('index.html', 
+      lists=TodoList.query.all(), 
+      active_list=TodoList.query.get(list_id),
+      todos=Todo.query.filter_by(list_id=list_id).order_by('id').all())
+
+@app.route('/')
 def index():
-    return render_template('index.html', todos=Todo.query.filter_by('list_id'=list_id).order_by('id').all())
+  return redirect(url_for('get_list_todos', list_id=1))
 
 if __name__ == '__main__':
   app.run(host='0.0.0.0', debug=True)
